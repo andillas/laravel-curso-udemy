934c0e94dffc7601a1c53d017fbad57718023426
Eugenio <andillas@yahoo.es>
2019-02-21 17:21:20 +0100

    Initial commit.

---
+++ probando-laravel/app/Http/Controllers/HuertoController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HuertoController extends Controller
+{
+    //
+    private $arr_hortalizas = ['Tomates', 'Pepinos', 'Guindillas', 'Cebollas', 'Lechugas', 'Espinacas', 'Sandías', 'Melones', 'Patatas'];
+
+    public function index(){
+        return view('huerto.index')
+            ->with('hortalizas', $this->arr_hortalizas);
+    }
+
+    public function reversedHortalizas(){
+        $reversed_hortalizas = array_reverse($this->arr_hortalizas);
+        return view('huerto.index')
+            ->with('hortalizas', $reversed_hortalizas);
+    }
+
+    public function tomates(){
+        return '<h2>Tomates</h2>';
+    }
+
+    public function cebollas(){
+        return '<h2>Cebollas</h2>';
+    }
+}
